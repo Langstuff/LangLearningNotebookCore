9af56163cd5507a253ba9e583638e32d490b558c
Raiden Fumo <herrscher.of.sleeping@protonmail.com>
2023-11-10 13:44:18 +0100

    Markdig experiments

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -2,6 +2,8 @@ using NotebookLua;
 using NotebookDatabase;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
+using Markdig;
+using Markdig.Syntax;
 
 static void setupDatabase() {
     using (var context = new NotebookContext())
@@ -22,39 +24,73 @@ static void setupDatabase() {
 
 setupDatabase();
 
-var nb1 = new LuaNotebook("My Notebook");
+var pipeline = new MarkdownPipelineBuilder()
+    .UseAdvancedExtensions()
+    // .UseGridTables()
+    .UsePipeTables()
+    // .EnableTrackTrivia()
+    .Build();
 
-nb1.Execute("lua", @"
-response = AI(
-    'Give me JSON array with pairs question-answer, questions being 5 most common words for food ingredients in Japanese and answers being English translations.',
-    'Example response [{""front"": ""肉"", ""back"": ""meat""}]'
-)
-io.write('A\n')
-print(response)
-io.write('B\n')
-io.write(response)
-io.write('C\n')
-ok, response_json = pcall(json.decode, response)
-io.write('D\n')
-if not ok then
-  io.write(response_json)
-else
-  io.write('Okay, doing the thing\n')
-  io.write(type(response_json) .. '\n')
-  save_deck('jp-en food deck', response_json)
-end
-");
-// nb1.Execute("lua", "print(response)");
-// nb1.Execute("lua", "io.write(response)");
-// nb1.Execute("lua", "response_json = json.decode(encode)");
-// nb1.Execute("lua", "save_deck('deck1', response_json)");
+var text = @"
+This is a text with some *emphasis*
+test
+test
+And a table:
 
-// nb1.Execute("lua", "response_json = json.decode(response)");
+| Column 1 | Column 2 |
+|----------|----------|
+| Test     | Test 2   |
+| Test     | Test 2   |
+
+";
+
+var document = Markdown.Parse(text, pipeline);
+
+foreach (var node in document.Descendants())
+{
+    Console.WriteLine(node.ToString());
+}
+
+
+Console.WriteLine(document.ToHtml(pipeline));
+
+// Console.WriteLine(Markdown.ToHtml(text, pipeline));
+
+
+
+// prints: <p>This is a text with some <em>emphasis</em></p>
+
+// var nb1 = new LuaNotebook("My Notebook");
+
+// nb1.lua.LoadCLRPackage();
 // nb1.Execute("lua", @"
-// for i = 1, #response_json do
-//   print(response_json[i].question .. ' | ' .. response_json[i].answer)
+// local NotebookDatabase = import('NotebookDatabase', 'NotebookDatabase')
+
+// for k, v in pairs(NotebookDatabase) do
+//     io.write(k .. ' : ' .. tostring(v) .. '\n')
 // end
 // ");
-// nb1.Execute("ai", @"Give me JSON array with pairs question-answer, questions
-// being 50 most common words for food ingredients in Czech and answers being English translations.
-// Response should be only JSON array and nothing else.");
+
+// nb1.Execute("lua", @"
+// response = AI(
+//     'Give me JSON array with pairs question-answer, questions being 50 most common words for food ingredients in Czech and answers being English translations.',
+//     'Follow example response and give only JSON. Example response [{""front"": ""sůl"", ""back"": ""salt""}]. JSON should be compressed, not indented.'
+// )
+// io.write('\nA\n')
+// print(response)
+// ");
+
+// nb1.Execute("lua", @"
+// io.write('\nB\n')
+// io.write(response)
+// io.write('\nC\n')
+// ok, response_json = pcall(json.decode, response)
+// io.write('\nD\n')
+// if not ok then
+//   io.write(response_json)
+// else
+//   io.write('Okay, doing the thing\n')
+//   io.write(type(response_json) .. '\n')
+//   save_deck('cz-en food deck', response_json)
+// end
+// ");
